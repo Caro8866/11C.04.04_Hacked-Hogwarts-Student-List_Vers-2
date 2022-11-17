320c5b376ac5546a58630a3e453621b98d803e7a
Caroline Thostrup <caro342h@stud.kea.dk>
2022-11-17 21:39:43 +0100

    data fetch + student object

--- js/main.js
+++ js/main.js
@@ -1,1 +1,30 @@
 "use strict";
+
+window.addEventListener("DOMContentLoaded", init);
+
+const studentJsonURL = "https://petlatkea.dk/2021/hogwarts/students.json";
+
+let allStudents = [];
+let studentList;
+
+//* Student Prototype Object
+const StudentObj = {
+  firstName: "-unknown-",
+  lastName: "-unknown-",
+  middleName: "-unknown-",
+  nickName: "-unknown-",
+  gender: "",
+  image: ".jpg",
+  house: "",
+};
+
+function init() {
+  console.log("Initial start");
+  loadJsonData();
+}
+
+function loadJsonData() {
+  fetch(studentJsonURL)
+    .then((response) => response.json())
+    .then((jsonData) => prepareObjects(jsonData));
+}
